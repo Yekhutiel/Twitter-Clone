482db4c4f80ee8ebcd1800298ac688cc3aacde7a
Questy Kuti <yekhutiel@gmail.com>
2025-01-13 13:01:27 +0100

    set up .env file and Mongoose connection

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
 import express from "express";
+import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import connectMongoDB from "./db/connectMongoD.js";
+
+dotenv.config();
 
 // create express app
 const app = express();
+const PORT = process.env.PORT || 8000;
 
 // reroute every search to extensions starting with '/api/auth' to 'authroutes
 app.use("/api/auth", authRoutes);
 
 // listen for conncetions on specified port
-app.listen(8000, () => {
-    console.log("Server is running on port 8000");
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    connectMongoDB();
 });
